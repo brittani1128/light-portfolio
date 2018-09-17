9778b3785873b7e0a46f802bd3eff4f0c2bf65bc
Brittani <brittani1128@gmail.com>
2018-09-16 23:14:28 -0500

    added materialize files, fixed cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.parallax');
+//     var instances = M.Parallax.init(elems, options);
+//   });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+
+
 document.querySelector(".card-flip").classList.toggle("flip");
 
-/* 
- * Holder.js for demo image
- * Just for demo purpose
- */
-// Holder.addTheme('gray', {
-//   bg: '#777',
-//   fg: 'rgba(255,255,255,.75)',
-//   font: 'Helvetica',
-//   fontweight: 'normal'
-// });
+
